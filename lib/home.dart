f10499efe7f0a4555073212bcc5a0e35f4ce21c7
natayudarsi <natayudarsi@gmail.com>
2020-05-22 17:50:51 +0700

    screen 1 and 2

---
+++ lib/home.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:info_napza/widget/bottom_navbar.dart';
+import 'package:info_napza/details_screen.dart';
+import 'package:info_napza/home_page.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int _index = 1;
+  final List<Widget> _children = [
+    DetailsScreen(),
+    MyHomePage()
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context)
+        .size;
+    return Scaffold(
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        height: 80,
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            BottomNavItem(
+              title: "Jenis",
+              svgScr: "assets/icons/calendar.svg",
+              press: () {
+                setState(() {
+                  _index = 0;
+                });
+              },
+              isActive: _index == 0 ,
+            ),
+            BottomNavItem(
+              title: "Home",
+              svgScr: "assets/icons/home.svg",
+              press: () {
+                setState(() {
+                  _index = 1;
+                });
+              },
+              isActive: _index==1,
+            ),
+            BottomNavItem(
+              title: "Kuis",
+              svgScr: "assets/icons/Settings.svg",
+              press: () {
+                setState(() {
+                  _index = 2;
+                });
+              },
+              isActive: _index == 2,
+            ),
+          ],
+        ),
+      ),
+    body: _children[_index],
+    );
+  }
+
+}
---
+++ lib/widget/bottom_navbar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class BottomNavItem extends StatelessWidget {
+  final String svgScr;
+  final String title;
+  final Function press;
+  final bool isActive;
+  const BottomNavItem({
+    Key key,
+    this.svgScr,
+    this.title,
+    this.press,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SvgPicture.asset(
+            svgScr,
+            color: isActive ? Color(0xFFE68342) : Color(0xFF222B45),
+          ),
+          Text(
+            title,
+            style: TextStyle(color: isActive ? Color(0xFFE68342) : Color(0xFF222B45)),
+          ),
+        ],
+      ),
+    );
+  }
+}
