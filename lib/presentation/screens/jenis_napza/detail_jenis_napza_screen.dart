72458f295c1d6af6f3ee91b2fc4dc0490c3a9003
natayudarsi <natayudarsi@gmail.com>
2020-09-12 12:49:42 +0700

    [FIX] : change design screen jenis napza

--- lib/presentation/screens/jenis_napza/detail_jenis_napza_screen.dart
+++ lib/presentation/screens/jenis_napza/detail_jenis_napza_screen.dart
@@ -1,96 +1,235 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailJenisNapzaScreen extends StatelessWidget {
+  final String name;
   final String napza;
   final List efek;
+  final String picture;
 
-  const DetailJenisNapzaScreen({Key key, this.napza, this.efek,}) : super(key: key);
+  const DetailJenisNapzaScreen({Key key, this.napza, this.efek, this.name, this.picture})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     var efekList = <Widget>[];
-    for(var text in efek) {
+    for (var text in efek) {
       efekList.add(UnorderedListItem(text));
-      efekList.add(SizedBox(height: 5.0,));
+      efekList.add(SizedBox(
+        height: 5.0,
+      ));
     }
 
     return Scaffold(
-       body: SafeArea(
-         child: Stack(
-           children: <Widget>[
-             Column(
-               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(32),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(height: size.height * 0.3,),
-
-                      // Text(
-                      //   'Audio Learner',
-                      //   style: TextStyle(
-                      //     fontFamily: 'TTCommons',
-                      //     fontSize: 52,
-                      //     fontWeight: FontWeight.w500
-                      //   ),
-                      //   textAlign: TextAlign.left,
-                      // ),
-                      Divider(color: Colors.black38),
-                      SizedBox(
-                        width:size.width * 0.9,
-                        height: size.height * 0.5,
-                        child: SingleChildScrollView(
-                          // child: Text(
-                          //   // InfoGayaBelajarString.ciri_ciri,
-                          //   style: TextStyle(
-                          //     fontFamily: 'TTCommons',
-                          //     fontSize: 16,
-                          //     fontWeight: FontWeight.w400
-                          //   ),
-                          //   textAlign: TextAlign.left,
-                          // ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(napza,
-                                style: TextStyle(
-                                  fontSize: 16
-                                ),     
-                              ),
-                              // Text('Ciri-ciri gaya belajar visual',
-                              //   style: TextStyle(
-                              //     fontSize: 16
-                              //   ),
-                              // ),
-                              Column(
-                                children: efekList,
-                              ),
-                              
-                            ],
-                          ),
-                        )
-                      ),
-                      Divider(color: Colors.black38,)
-                    ],
+        body: Stack(
+      children: <Widget>[
+        Container(
+            height: size.height * .45,
+            child: Image.asset(
+              picture,
+              width: size.width,
+              fit: BoxFit.fill,
+            ),
+          ),
+        Container(
+          padding: EdgeInsets.only(bottom: size.height * 0.13),
+          height: size.height * .45,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              name,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  .copyWith(fontWeight: FontWeight.w600, color: Colors.white),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Container(
+              margin: EdgeInsets.only(
+                  top: size.height * 0.05, left: size.width * 0.05),
+              alignment: Alignment.center,
+              height: 52,
+              width: 52,
+              decoration:
+                  BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+              // child: SvgPicture.asset('assets/icons/menu.svg'),
+              child: Icon(
+                IconData(58848, fontFamily: 'MaterialIcons', matchTextDirection: true)
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.symmetric(
+                vertical: size.height * 0.02, horizontal: size.width * 0.07),
+            height: size.height * 0.65,
+            decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.vertical(top: Radius.circular(40))),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                        text: 'Apa sih ',
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                              fontWeight: FontWeight.w600,
+                            ),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: name,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline4
+                                  .copyWith(
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.black)),
+                          TextSpan(text: ' itu ?')
+                        ]),
                   ),
-                ),
-               ]
-             ),
-            //  Container(
-            //    width: size.width,
-            //    height: size.height * 0.3,
-            //    child: Image.asset(image)
-            //   )
-           ],
-         )
-      )
-    );
+                  SizedBox(
+                    height: size.height * 0.01,
+                  ),
+                  Text(
+                    napza,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.03,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: 'Efek ',
+                        style: Theme.of(context).textTheme.headline4.copyWith(
+                              fontWeight: FontWeight.w600,
+                            ),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: name,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline4
+                                  .copyWith(
+                                      fontWeight: FontWeight.w800,
+                                      color: Colors.black)),
+                        ]),
+                  ),
+                  SizedBox(
+                    height: size.height * 0.01,
+                  ),
+                  Column(
+                    children: efekList,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        //  Column(
+        //    children: <Widget>[
+        //     Padding(
+        //       padding: EdgeInsets.all(32),
+        //       child: Column(
+        //         crossAxisAlignment: CrossAxisAlignment.start,
+        //         children: <Widget>[
+        //           SizedBox(height: size.height * 0.3,),
+        //           SizedBox(
+        //             width:size.width * 0.9,
+        //             height: size.height * 0.5,
+        //             child: SingleChildScrollView(
+        //               child: Column(
+        //                 crossAxisAlignment: CrossAxisAlignment.start,
+        //                 children: <Widget>[
+        //                   RichText(
+        //                     text: TextSpan(
+        //                       text: 'Apa sih ',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .headline4
+        //                           .copyWith(
+        //                               fontWeight: FontWeight.w600,),
+        //                       children: <TextSpan>[
+        //                         TextSpan(
+        //                           text: name,
+        //                           style : Theme.of(context)
+        //                           .textTheme
+        //                           .headline4
+        //                           .copyWith(
+        //                               fontWeight: FontWeight.w800, color: Colors.black)
+        //                         ),
+        //                         TextSpan(
+        //                           text: ' itu ?'
+        //                         )
+        //                       ]
+        //                     ),
+        //                   ),
+        //                   SizedBox(
+        //                     height: size.height * 0.01,
+        //                   ),
+        //                   Text(napza,
+        //                     style: TextStyle(
+        //                       fontSize: 16
+        //                     ),
+        //                   ),
+        //                   SizedBox(
+        //                     height: size.height * 0.03,
+        //                   ),
+        //                   RichText(
+        //                     text: TextSpan(
+        //                       text: 'Efek ',
+        //                       style: Theme.of(context)
+        //                           .textTheme
+        //                           .headline4
+        //                           .copyWith(
+        //                               fontWeight: FontWeight.w600,),
+        //                       children: <TextSpan>[
+        //                         TextSpan(
+        //                           text: name,
+        //                           style : Theme.of(context)
+        //                           .textTheme
+        //                           .headline4
+        //                           .copyWith(
+        //                               fontWeight: FontWeight.w800, color: Colors.black)
+        //                         ),
+        //                       ]
+        //                     ),
+        //                   ),
+        //                   SizedBox(
+        //                     height: size.height * 0.01,
+        //                   ),
+        //                   Column(
+        //                     children: efekList,
+        //                   ),
+
+        //                 ],
+        //               ),
+        //             )
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //    ]
+        //  ),
+        //  Container(
+        //    width: size.width,
+        //    height: size.height * 0.3,
+        //    child: Image.asset(image)
+        //   )
+      ],
+    ));
   }
-
 }
-
 
 class UnorderedListItem extends StatelessWidget {
   UnorderedListItem(this.text);
@@ -101,9 +240,15 @@ class UnorderedListItem extends StatelessWidget {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text("   • "),
+        Text(
+          "  • ",
+          style: TextStyle(fontSize: 20),
+        ),
         Expanded(
-          child: Text(text),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 16),
+          ),
         ),
       ],
     );
--- lib/presentation/screens/jenis_napza/jenis_napza_screen.dart
+++ lib/presentation/screens/jenis_napza/jenis_napza_screen.dart
@@ -4,12 +4,23 @@ import 'package:info_napza/category_card.dart';
 import 'package:info_napza/common/routing/routes.dart';
 import 'package:info_napza/model/jenis_napza_model.dart';
 
-class JenisNapzaScreen extends StatefulWidget{
+class JenisNapzaScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _JenisNapzaScreenState();
 }
 
-class _JenisNapzaScreenState extends State<JenisNapzaScreen>{
+class _JenisNapzaScreenState extends State<JenisNapzaScreen> {
+  List<String> images = ['assets/images/ganja.jpeg', 'assets/images/kokain.jpg', 'assets/images/ekstasi.png', 'assets/images/morphine-pills.jpg', 'assets/images/morphine-pills.jpg'];
+  List<String> name = ['Sabu', 'Cocain', 'Ekstasy', 'Morfin', 'Obat Penenang'];
+  List<String> detailName = [shabu, cocain, ekstasy, morfin, obatPenenang];
+  List<dynamic> efekNapza = [
+    efekShabu,
+    efekCocain,
+    efekEkstasy,
+    efekMorfin,
+    efekObatPenenang
+  ];
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -17,78 +28,193 @@ class _JenisNapzaScreenState extends State<JenisNapzaScreen>{
       body: Stack(
         children: <Widget>[
           Container(
+            height: size.height * .45,
             color: Colors.pink[100],
+            child: Image.asset(
+              'assets/images/Artwork-2.jpg',
+              width: size.width,
+              fit: BoxFit.fill,
+            ),
           ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: size.height * 0.01,bottom: size.height * 0.05),
-                    alignment: Alignment.center,
-                      child: Text(
-                      'Jenis-jenis Napza',
-                      style: Theme.of(context)
-                            .textTheme
-                            .display1
-                            .copyWith(fontWeight: FontWeight.w900),
-                    ),
-                  ),
-                  // SearchBar(),
-                  Expanded(
-                    child: GridView.count(
-                      crossAxisCount: 2,
-                      childAspectRatio: .85,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20,
-                      children: <Widget>[
-                        CategoryCard(
-                          title: "Cocain",
-                          pictureSrc: "assets/images/cocain.jpeg",
-                          press: () {
-                            Map payload = {
-                              'napza': cocain,
-                              'efek': efekCocain,
-                            };
-                            Navigator.of(context).pushNamed(Routes.detailJenisNapza, arguments: payload);
-                          },
-                        ),
-                        CategoryCard(
-                          title: "Ectasy",
-                          pictureSrc: "assets/images/ectasy.jpeg",
-                          press: () {
-                          },
-                        ),
-                        CategoryCard(
-                          title: "Ganja",
-                          pictureSrc: "assets/images/ganja.jpeg",
-                          press: () {
-                          },
-                        ),
-                        CategoryCard(
-                          title: "Morphine",
-                          pictureSrc: "assets/images/morphine.jpeg",
-                          press: () {
-                          },
-                        ),
-                        CategoryCard(
-                          title: "Obat Penenang",
-                          pictureSrc: "assets/images/obat-penenang.jpeg",
-                          press: () {
-                          },
-                        ),
-                      ],
-                    )
-                  )
-                ],
+          Align(
+            alignment: Alignment.topLeft,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                margin: EdgeInsets.only(
+                    top: size.height * 0.05, left: size.width * 0.05),
+                alignment: Alignment.center,
+                height: 52,
+                width: 52,
+                decoration:
+                    BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                // child: SvgPicture.asset('assets/icons/menu.svg'),
+                child: Icon(IconData(58848,
+                    fontFamily: 'MaterialIcons', matchTextDirection: true)),
               ),
-            )
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: size.height * 0.65,
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+              ),
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.symmetric(
+                        horizontal: size.width * 0.1,
+                        vertical: size.height * 0.01),
+                    height: size.height * .1,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(50),
+                      child: InkWell(
+                        onTap: () {
+                          Map payload = {
+                            'napza': detailName[index],
+                            'efek': efekNapza[index],
+                            'name': name[index],
+                            'picture': images[index]
+                          };
+                          Navigator.of(context).pushNamed(
+                              Routes.detailJenisNapza,
+                              arguments: payload);
+                        },
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: Image.asset(
+                                images[index],
+                                width: size.width,
+                                // height: 50,
+                                fit: BoxFit.none,
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Container(
+                                margin:
+                                    EdgeInsets.only(left: size.width * 0.03),
+                                alignment: Alignment.centerLeft,
+                                height: 52,
+                                width: size.width * 0.7,
+                                // decoration: BoxDecoration(
+                                //     color: Colors.red, shape: BoxShape.circle),
+                                child: Text(
+                                  name[index],
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline5
+                                      .copyWith(
+                                          fontWeight: FontWeight.w900,
+                                          color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Container(
+                                margin:
+                                    EdgeInsets.only(right: size.width * 0.03),
+                                alignment: Alignment.center,
+                                height: 52,
+                                width: 52,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    shape: BoxShape.circle),
+                                child: Icon(IconData(58849,
+                                    fontFamily: 'MaterialIcons',
+                                    matchTextDirection: true)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           )
+          // SafeArea(
+          //   child: Padding(
+          //     padding: const EdgeInsets.symmetric(horizontal: 20),
+          //     child: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.start,
+          //       children: <Widget>[
+          //         Container(
+          //           margin: EdgeInsets.only(top: size.height * 0.01,bottom: size.height * 0.05),
+          //           alignment: Alignment.center,
+          //             child: Text(
+          //             'Jenis-jenis Napza',
+          //             style: Theme.of(context)
+          //                   .textTheme
+          //                   .display1
+          //                   .copyWith(fontWeight: FontWeight.w900),
+          //           ),
+          //         ),
+          //         // SearchBar(),
+          //         Expanded(
+          //           child: GridView.count(
+          //             crossAxisCount: 2,
+          //             childAspectRatio: .85,
+          //             crossAxisSpacing: 20,
+          //             mainAxisSpacing: 20,
+          //             children: <Widget>[
+          //               CategoryCard(
+          //                 title: "Cocain",
+          //                 pictureSrc: "assets/images/cocain.jpeg",
+          //                 press: () {
+          //                   Map payload = {
+          //                     'napza': cocain,
+          //                     'efek': efekCocain,
+          //                   };
+          //                   Navigator.of(context).pushNamed(Routes.detailJenisNapza, arguments: payload);
+          //                 },
+          //               ),
+          //               CategoryCard(
+          //                 title: "Ectasy",
+          //                 pictureSrc: "assets/images/ectasy.jpeg",
+          //                 press: () {
+          //                 },
+          //               ),
+          //               CategoryCard(
+          //                 title: "Ganja",
+          //                 pictureSrc: "assets/images/ganja.jpeg",
+          //                 press: () {
+          //                 },
+          //               ),
+          //               CategoryCard(
+          //                 title: "Morphine",
+          //                 pictureSrc: "assets/images/morphine.jpeg",
+          //                 press: () {
+          //                 },
+          //               ),
+          //               CategoryCard(
+          //                 title: "Obat Penenang",
+          //                 pictureSrc: "assets/images/obat-penenang.jpeg",
+          //                 press: () {
+          //                 },
+          //               ),
+          //             ],
+          //           )
+          //         )
+          //       ],
+          //     ),
+          //   )
+          // )
         ],
       ),
     );
   }
-
 }
