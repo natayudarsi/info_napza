decad3d232fdbc44f6bfa036e3bb51c8697c7c1f
natayudarsi <natayudarsi@gmail.com>
2020-09-12 12:46:44 +0700

    [FIX] : change information data

--- lib/model/jenis_napza_model.dart
+++ lib/model/jenis_napza_model.dart
@@ -1,4 +1,4 @@
-String ekstasy = 'Ekstasy\nDikenal dengan nama : Inex, I, Kancing Drug, Yuppie Drug, Essence, Clarity, Butterfly ,Black Heart, dll.\nBentuk\t: Berupa table dan kapsul\nWarna\t: bermacam-macam\nPenggunaan\t: ditelan\nEfek :';
+String ekstasy = 'Ekstasy Dikenal dengan nama : Inex, I, Kancing Drug, Yuppie Drug, Essence, Clarity, Butterfly ,Black Heart, dll.\nBentuk\t: Berupa table dan kapsul\nWarna\t: bermacam-macam\nPenggunaan\t: ditelan\nEfek :';
     
 List efekEkstasy = ['Timbul rasa gembira secara berlebihan. Banyak orang mengkonsumsi ekstasy untuk tujuan bersenang-senang. Ekstasy banyak digunakan oleh anak-anak muda agar dapat berpesta di diskotik sepanjang malam. Karena saking gembiranya kadang-kadang sampai lepas kendali sehingga tidak malu-malu melakukan pesta seks.',
                     'Merasa cemas',
@@ -7,7 +7,7 @@ List efekEkstasy = ['Timbul rasa gembira secara berlebihan. Banyak orang mengkon
                     'Mengalami keringat dan gemetaran',
                     'Susah tidur'];
 
-String cocain = 'Cocain\nBerasal dari tanaman yang banyak dijumpai di Columbia di Amerika Latin.\nBentuk\t: berupa bubuk, daun coca , buah coca , cocain kistal.\nWarna\t: -Cairan berwarna putih/tidak berwarna\n -Kristal berwarna putih\n -Tablet berwarna putih\n -Bubuk/serbuk seperti tepung\nPenggunaan\t: Dengan cara menghirup melalui hidung dengan menggunakan alat penyedot (sedotan) atau dapat juga dibakar bersama-sama dengan tembakau (rokok), ditelan bersama minuman, atau disuntikan pada pembuluh darah.\nEfek\t: ';
+String cocain = 'Cocain Berasal dari tanaman yang banyak dijumpai di Columbia di Amerika Latin.\nBentuk\t: berupa bubuk, daun coca , buah coca , cocain kistal.\nWarna\t: -Cairan berwarna putih/tidak berwarna\n -Kristal berwarna putih\n -Tablet berwarna putih\n -Bubuk/serbuk seperti tepung\nPenggunaan\t: Dengan cara menghirup melalui hidung dengan menggunakan alat penyedot (sedotan) atau dapat juga dibakar bersama-sama dengan tembakau (rokok), ditelan bersama minuman, atau disuntikan pada pembuluh darah.\nEfek\t: ';
 
 List efekCocain =['Tidak bergairah bekerja', 
     'Tidak bisa tidur',
@@ -16,7 +16,7 @@ List efekCocain =['Tidak bergairah bekerja',
     'Berbuat dan berfikir tanpa tujuan',
     'Merasa gelisah dan cemas berlebihan'];
 
-String morfin= 'Morfin dan Heroin\nNama lain putaw, smack, junk, horse, H, PT, Etep, Bedak Putih\nBentuk\t: Berupa Serbuk\nWarna\t: Putih, abu-abu, kecoklatan hingga coklat tua\nPenggunaan\t: Dengan cara menghirup asapnya setelah bubuk heroin dibakar diatas kertas timah pembungkus rokok (sniffing­) atau dengan menyuntikkannya langsung ke pembuluh darah setelah heroin dilarutkan dalam air.\nEfek\t:';
+String morfin= 'Morfin dan Heroin Nama lain putaw, smack, junk, horse, H, PT, Etep, Bedak Putih\nBentuk\t: Berupa Serbuk\nWarna\t: Putih, abu-abu, kecoklatan hingga coklat tua\nPenggunaan\t: Dengan cara menghirup asapnya setelah bubuk heroin dibakar diatas kertas timah pembungkus rokok (sniffing­) atau dengan menyuntikkannya langsung ke pembuluh darah setelah heroin dilarutkan dalam air.\nEfek\t:';
 
 List efekMorfin = ['Menimbulkan rasa mengantuk, lesu, penampilan “dungu” jalan mengambang.',
     'Rasa sakit seluruh badan',
@@ -25,13 +25,13 @@ List efekMorfin = ['Menimbulkan rasa mengantuk, lesu, penampilan “dungu” jal
     'Matanya berair dan hidungnya selalu ingusan',
     'Problem pada kesehatan : bengkak pada daerah menyuntik, tetanus, HIV/AIDS, Hepatitis B dan C, problem jantung, dada dan paru-paru, serta sulit buang air besar, pada wanita mengganggu sirkulasi menstruasi.'];
 
-String obatPenenang = 'Obat penenang\n(obat tidur, pil koplo, BK, Nipam, Valium, Lexotan dan lain-lain)\nBentuk\t: Tablet, Kapsul, serbuk\nCara penggunaan\t: ditelan secara langsung\nEfek\t :';
+String obatPenenang = 'Obat penenang (obat tidur, pil koplo, BK, Nipam, Valium, Lexotan dan lain-lain)\nBentuk\t: Tablet, Kapsul, serbuk\nCara penggunaan\t: ditelan secara langsung\nEfek\t :';
     
 List efekObatPenenang = ['Bicara jadi pelo, memperlambat Respons fisik, mental dan emosi. Dalam dosis tinggi akan membuat pengguna tidur, kemudian akan menimbulkan perasaan cemas, sensitive dan marah.',
     'Penggunaan campuran dengan alkohol dapat berakibat kematian', 
     'Gejala putus zat berakibat halusinasi buruk, bingung'];
 
-String shabu = 'Shabu\nDikenal dengan nama : Kristal, Ubas, SS, Mecin.\nBentuk\t: Berupa Kristal\nWarna\t: Putih\nPenggunaan\t: dibakar dengan menggunakan alluminium foil dan asapnya dihirup melalui hidung. Dibakar dengan menggunakan botol kaca khusus (bong) dan disuntikkan.\nEfek\t: ';
+String shabu = 'Shabu Dikenal dengan nama : Kristal, Ubas, SS, Mecin.\nBentuk\t: Berupa Kristal\nWarna\t: Putih\nPenggunaan\t: dibakar dengan menggunakan alluminium foil dan asapnya dihirup melalui hidung. Dibakar dengan menggunakan botol kaca khusus (bong) dan disuntikkan.\nEfek\t: ';
     
 List efekShabu = ['Badannya merasa lebih kuat dan energik (meningkatkan stamina)',
     'Tidak mau diam (hiperaktif)',
